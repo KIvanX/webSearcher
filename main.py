cb3a8b216e14576eb83c8db1122e90cef1ac76f7
Ivan Kosach <kosaci74@gmail.com>
2025-03-17 11:03:25 +0300

    task 1 is completed

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
+import json
+import time
+from selenium import webdriver
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.common.by import By
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+caps = ChromeOptions()
+caps.page_load_strategy = "eager"
+driver = webdriver.Chrome(options=caps)
+
+pages, i = [], 1
+while len(pages) < 100:
+    driver.get(f'https://habr.com/ru/search/page{i}/?q=андроид&target_type=posts&order=relevance')
+    time.sleep(2)
+    i += 1
+
+    urls = [e.find_element(By.CLASS_NAME, 'tm-title__link').get_attribute('href') for e in driver.find_elements(By.TAG_NAME, 'article')]
+    for page_url in urls[:3]:
+        try:
+            driver.get(page_url)
+            time.sleep(1)
+
+            content = driver.find_element(By.CLASS_NAME, 'tm-article-body').text
+            if len(content) > 3000:
+                pages.append({'url': page_url, 'content': content})
+                print(len(pages))
+        except:
+            pass
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+index_text = '\n'.join([f'page_{i}.txt - {page["url"]}' for i, page in enumerate(pages)])
+with open('index.txt', 'w', encoding='utf-8') as f:
+    f.write(index_text)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for i, page in enumerate(pages):
+    with open(f'static/page_{i}.txt', 'w', encoding='utf-8') as f:
+        f.write(page['content'])
